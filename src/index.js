041a288b38c762466fce6adbb48799289c485792
sadiebennett1 <sadiebenn1@gmail.com>
2019-04-03 22:36:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -49,41 +49,23 @@ dogBreedUL.addEventListener('click', (event) => {
   }
 })
 
-let choicesBox = document.getElementById('breed-dropdown');
 
-choicesBox.addEventListener('change', (event) => {
-  if (event.target.tagName === 'OPTION') {
-    let usersOption = event.target.value
+document.addEventListener('change', (event) => {
+  dogBreedUL.innerHTML = ''
+  let usersOption = event.target.value
     fetch(breedUrl)
       .then((response) => {
         return response.json()
       }).then((object) => {
         let messageValues = Object.values(object)[1]
-
         let breedKeys = Object.keys(messageValues)
-        if (usersOption == 'a') {
-          const startsWithA = breedKeys.filter((breed) => breed.startsWith("a"));
-          startsWithA.forEach((breed) => {
-            dogBreedUL.innerHTML += createDogBreedListItem(breed)})
-        }
-        if (usersOption == 'b') {
-          const startsWithB = breedKeys.filter((breed) => breed.startsWith("b"));
-          startsWithB.forEach((breed) => {
-            dogBreedUL.innerHTML += createDogBreedListItem(breed)})
-        }
-        if (usersOption == 'c') {
-          const startsWithC = breedKeys.filter((breed) => breed.startsWith("c"));
-          startsWithC.forEach((breed) => {
-            dogBreedUL.innerHTML += createDogBreedListItem(breed)})
-        }
-        if (usersOption == 'd') {
-        const startsWithD = breedKeys.filter((breed) => breed.startsWith("d"));
-        startsWithD.forEach((breed) => {
-          dogBreedUL.innerHTML += createDogBreedListItem(breed)})
-        }
-
+        breedKeys.forEach((breed) => {
+          if (breed.startsWith(usersOption)) {
+            dogBreedUL.innerHTML += createDogBreedListItem(breed)}
+        })
       })
-
     }
-  })
+  )
+
+
 // let usersOption = choices.options[choices.selectedIndex].value;
